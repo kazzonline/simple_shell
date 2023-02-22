eae517101d29f5af64dfdf86b6a4a375c3e7e163
Doctware <yusclever9@gmail.com>
2023-02-22 09:02:54 +0100

    pass the betty test

---
+++ parser.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+
+/**
+ * main - print shell definition.
+ * Return: 0 as success
+ */
+
+int main(void);
+{
+	printf(" Shell is an interpriter between man and the machine ");
+
+	return (0);
+}
